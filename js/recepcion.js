4ba4724dd39518a93e102597e4eb2a86759f9c7a
BahlMartin <bahlmartinexactas@gmail.com>
2025-08-23 04:18:07 -0300

    modificacion de nombre archivo,obj turno, y display de medico

---
+++ js/recepcion.js
@@ -0,0 +1,25 @@
+import { createDoc } from "./couchDBUtils.js";
+
+import { DATABASE_TURNOS } from "./propiedades.js";
+
+addEventListener("DOMContentLoaded",()=>{
+    btmagregarpaciente.addEventListener("click",agregarTurnoPaciente);
+});
+
+function agregarTurnoPaciente() {
+    var diayfecha = new Date();
+
+    //creacion de objeto de datos del turno del paciente
+    let turno = {
+        "nombre": nombre.value,
+        "apellido": apellido.value,
+        "dni": dni.value,
+        "obrasocial": obrasocial.value,
+        "especialidad": especialidad.value,
+        "fecharegistro": diayfecha.toLocaleDateString(),
+        "horaregistro": diayfecha.toLocaleTimeString(),
+        "estado": "pendiente"
+    }
+    createDoc(turno,DATABASE_TURNOS);
+    formulario.reset();
+}
